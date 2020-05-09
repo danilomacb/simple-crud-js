5c364fc4795a7e6de0aee6a76d0cf07a72027ad2
danilomacb <danilo.bakun@gmail.com>
2020-05-09 11:56:05 -0300

    finishing style

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Form, InputGroup, Button } from "react-bootstrap";
 
 function Home() {
   const [elements, setElements] = useState([]);
@@ -15,11 +16,27 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      {elements.map((element) => (
-        <div key={element._id}>{element.content}</div>
-      ))}
-    </div>
+    <Container className="mt-5">
+      <Row>
+        {elements.map((element) => (
+          <Col key={element._id} sm={6} md={4} lg={3}>
+            <Form>
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <Button variant={"danger"}>X</Button>
+                </InputGroup.Prepend>
+                <Form.Control type="text" defaultValue={element.content} />
+              </InputGroup>
+            </Form>
+          </Col>
+        ))}
+        <Col sm={6} md={4} lg={3}>
+          <Form>
+            <Form.Control type="text" placeholder="New" />
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
