3434ac30df7821ca236ca5358735aede0af624f1
danilomacb <danilo.bakun@gmail.com>
2020-05-21 10:41:56 -0300

    removing react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./components/Home";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
